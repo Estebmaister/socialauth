037ded3524752d6a1b684b67bee092dc7fa05ef4
Esteban Camargo <estebmaister@gmail.com>
2020-06-18 13:53:49 -0400

    🔧 editing path with process env variables

--- config.js
+++ config.js
@@ -5,11 +5,12 @@ require("dotenv").config();
 const SESSION_SECRET = process.env.SESSION_SECRET;
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-const P_URL = process.env.P_URL;
-const GITHUB_CALLBACK_URL =
-  P_URL || "http://127.0.0.1:3000/auth/github/callback";
+
+const GITHUB_CLIENT_ID =
+  process.env.LOCAL_CLIENT_ID || process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET =
+  process.env.LOCAL_CLIENT_SECRET || process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_CALLBACK_URL = process.env.LOCAL_URL || process.env.PROJECT_URL;
 
 module.exports = {
   SESSION_SECRET,
